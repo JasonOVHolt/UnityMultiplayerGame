48bc99070011c01e2f07e81898da54bc57fc59e5
DankKid <jmanholt929@gmail.com>
2024-10-06 13:53:15 -0400

    Updated file structure and got map selection working

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenManager : MonoBehaviour
 {
     bool showingHost = false, showingJoin = false, showingSettings = false;
     [SerializeField] GameObject hostSettings, joinSettings, settings;
-
+    [SerializeField] Image mapImage;
+    [SerializeField] List<Sprite> mapImages;
+    [SerializeField] int currentMap;
 
     // Start is called before the first frame update
-    void Begin()
+    void Awake()
     {
         hostSettings.SetActive(false);
         joinSettings.SetActive(false);
@@ -63,13 +68,34 @@ public class TitleScreenManager : MonoBehaviour
             settings.SetActive(false);
         }
 
-
     }
+
+
+    public void LeftMap()
+    {
+        currentMap -= 1;
+        if (currentMap < 0)
+            currentMap = mapImages.Count-1;
+        mapImage.sprite = mapImages[currentMap];
+    }
+
+    public void RightMap()
+    {
+        currentMap += 1;
+        if (currentMap >= mapImages.Count)
+            currentMap = 0;
+        mapImage.sprite = mapImages[currentMap];
+    }
+
 
     public void Quit() {
     Application.Quit();    
     }
 
-
+    public void HostGame()
+    {
+        string sceneName = (currentMap + 1).ToString();
+        SceneManager.LoadScene(sceneName);
+    }
 
 }
